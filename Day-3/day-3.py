2e77293f23f004d36d2b24bfead583be442a2957
Sumaiya <jahansumaiya49@gmail.com>
2023-01-07 20:21:39 +0600

    day3 to day5

---
+++ Day-3/day-3.py
@@ -0,0 +1,7 @@
+#pip install pandas
+#pip install scikit-learn
+#pip install tensorflow
+import pandas
+import sklearn
+import hashlib
+#import tensorflow(not supported for my laptop for GPU constraint)
---
+++ Day-5/day5.py
@@ -0,0 +1,17 @@
+# print("This for new line
+# but I gave enter.And it is showing error")
+
+# This is multi line comment
+# '''
+# '''
+
+# or 
+
+# """
+# """
+
+
+print("To solve the above problem, python have Escape Sequences like\nnow there is no error")
+
+print("Hello",6,7,sep="~",end="009\n")
+print("Hi")
